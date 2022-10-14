e8b01de60cec02c43217cbd62f6eb49ef6c0c65e
gaveinti <gaveinti>
2022-10-14 15:57:35 -0500

    Clases a implementar

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -2,39 +2,69 @@ package ec.edu.espol.workshops;
 import java.util.Scanner;
 public class CarInsurance {
 	
-	int age;
-	double km;
-	String maritalStatus;
-	char sex;
 	static int base = 500;
 	int result = 0;
 	
+	Cliente cliente = new Cliente();
 	
-	public void getData(){
+	
+	public static boolean haveLicense(char entrada) {
+		return entrada == 's';
+	}
+	
+	public static boolean haveValidEntry(char entrada) {
+		return entrada == 's' || entrada == 'n';
+	}
+	
+	public static boolean haveValidSexEntry(char entrada) {
+		return entrada == 'F' || entrada == 'M';
+	}
+	
+	public static boolean haveValidCivilStatus(String entrada) {
+		return entrada.contentEquals("Single") || entrada.contentEquals("Married");
+	}
+	
+	
+	
+	public static void getData(){
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Ingrese edad:");
-		this.age=Integer.parseInt(maritalStatus.nextLine());
+		
 	}
 	
 	
 	public int calculate() {
 		
-		if (this.sex=='M' && this.age<25 && !this.maritalStatus.equals("married") ) {
+		if (this.cliente.getSexo().equals(Sex.Male) && this.cliente.getEdad()<25 && !this.cliente.getMaritalStatus().equals(MaritalStatus.Single) ) {
 			this.result = base+ 1500;
 		}
 		
-		if (this.sex=='F' || this.maritalStatus.equals("married") ) {
+		if (this.cliente.getSexo().equals(Sex.Female) || this.cliente.getMaritalStatus().equals(MaritalStatus.Married )) {
 			this.result = base - 200;
 		}
-		if ( this.age>45 && this.age<65 ) {
+		if ( this.cliente.getEdad()>45 && this.cliente.getEdad()<65 ) {
 			this.result = base - 100;
 		}
 		
-		if (  this.age>80 ) {
+		if (  this.cliente.getEdad()>80 ) {
 			this.result = -1;
 		} 
 		
 		return this.result;
 	}
+	
+	public static void main(String []args){
+
+		Scanner entry = new Scanner(System.in);
+		boolean exit=false;
+		
+		
+		System.out.println("Bienvenido al sistema de calculo de la prima del seguro");
+		System.out.println("Tiene licencia valida de conducir?(s/n)");
+		char license=entry.next().charAt(0);
+		
+		
+	}
+	
 	
 }
